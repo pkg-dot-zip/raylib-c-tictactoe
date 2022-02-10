e837c071c644ba36db6bed2b55f0a0d3b0d9692d
Simon <dutchsimon9@gmail.com>
2022-02-10 13:50:41 +0100

    Basic refactor of template

--- sources/main.c
+++ sources/main.c
@@ -3,33 +3,52 @@
 #define SCREEN_WIDTH (800)
 #define SCREEN_HEIGHT (450)
 
-#define WINDOW_TITLE "Window title"
+#define WINDOW_TITLE "TicTacToe"
 
-int main(void)
-{
+Texture2D texture;
+
+void draw() {
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
+    const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
+    DrawTexture(texture, texture_x, texture_y, WHITE);
+
+    const char* text = "OMG! IT WORKS!";
+    const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
+    DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
+
+    EndDrawing();
+}
+
+void update(float deltaTime) {
+
+}
+
+void init() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
     SetTargetFPS(60);
 
-    Texture2D texture = LoadTexture(ASSETS_PATH"test.png"); // Check README.md for how this works
+    texture = LoadTexture(ASSETS_PATH"test.png"); // Check README.md for how this works
+}
+
+int deInit() {
+    UnloadTexture(texture);
+    CloseWindow();
+    return 0;
+}
+
+int main(void)
+{
+    init();
 
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
-        const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
-        DrawTexture(texture, texture_x, texture_y, WHITE);
-
-        const char* text = "OMG! IT WORKS!";
-        const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-        DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
-
-        EndDrawing();
+        draw();
+        update(GetFrameTime());
     }
 
-    CloseWindow();
-
-    return 0;
+    return deInit();
 }
