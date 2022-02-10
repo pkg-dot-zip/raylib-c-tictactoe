f47c1f48eede55f8fd4d6fd92a0bd54524820535
Simon <dutchsimon9@gmail.com>
2022-02-10 16:21:21 +0100

    Clean up

--- sources/main.c
+++ sources/main.c
@@ -5,12 +5,11 @@
 #define SCREEN_HEIGHT (450)
 
 #define WINDOW_TITLE "TicTacToe"
-
-Texture2D texture;
 const int distance = 40;
+const int fontSize = 20;
 
 typedef struct {
-    char* value;
+    char *value;
     int x;
     int y;
 } field;
@@ -22,24 +21,19 @@ void draw() {
 
     ClearBackground(RAYWHITE);
 
-    const int texture_x = GetScreenWidth() / 2 - texture.width / 2;
-    const int texture_y = GetScreenHeight() / 2 - texture.height / 2;
-    DrawTexture(texture, texture_x, texture_y, WHITE);
-
-    const char* text = "OMG! IT WORKS!";
-    const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-    DrawText(text, GetScreenWidth() / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
-
     // Draw board.
     for (int i = 0; i < sizeof(board) / sizeof(board[0]); ++i) {
-        DrawRectangleLines(GetScreenWidth() / 2 + board[i].x * distance, GetScreenHeight() / 2 + board[i].y * distance, distance, distance, BLACK);
+        DrawRectangleLines(GetScreenWidth() / 2 + board[i].x * distance, GetScreenHeight() / 2 + board[i].y * distance,
+                           distance, distance, BLACK);
     }
 
     // Draw inner values.
     for (int i = 0; i < sizeof(board) / sizeof(board[0]); ++i) {
         field value = board[i];
-        const char* textToDraw = value.value;
-        DrawText(textToDraw, GetScreenWidth() / 2 + value.x * distance, GetScreenHeight() / 2 + value.y * distance, 20, BLACK);
+        const char *textToDraw = value.value;
+        const Vector2 textSize = MeasureTextEx(GetFontDefault(), textToDraw, fontSize, 1);
+        DrawText(textToDraw, GetScreenWidth() / 2 + value.x * distance + textSize.x / 2,
+                 GetScreenHeight() / 2 + value.y * distance + textSize.y, fontSize, BLACK);
     }
 
     EndDrawing();
@@ -51,7 +45,8 @@ void update(float deltaTime) {
 
         for (int i = 0; i < sizeof(board) / sizeof(board[0]); ++i) {
             field value = board[i];
-            Rectangle rect = {GetScreenWidth() / 2 + value.x * distance, GetScreenHeight() / 2 + value.y * distance, distance, distance};
+            Rectangle rect = {GetScreenWidth() / 2 + value.x * distance, GetScreenHeight() / 2 + value.y * distance,
+                              distance, distance};
             if (CheckCollisionPointRec(pos, rect)) {
                 board[i].value = "X";
                 // TODO: Switch turn.
@@ -64,11 +59,10 @@ void init() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
     SetTargetFPS(60);
 
-    texture = LoadTexture(ASSETS_PATH"test.png"); // Check README.md for how this works.
-
+    // Init board.
     for (int x = 0; x < BOARD_SIZE / 3; ++x) {
         for (int y = 0; y < BOARD_SIZE / 3; ++y) {
-            char* text = "h";
+            char *text = "h";
             field value = {text, x, y};
             board[y * 3 + x] = value;
         }
@@ -76,17 +70,14 @@ void init() {
 }
 
 int deInit() {
-    UnloadTexture(texture);
     CloseWindow();
     return 0;
 }
 
-int main(void)
-{
+int main(void) {
     init();
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
         draw();
         update(GetFrameTime());
     }
