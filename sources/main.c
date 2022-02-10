ad9a25abd374b083edfcc480b04515e58f6216d2
Simon <dutchsimon9@gmail.com>
2022-02-10 14:32:45 +0100

    We can now click stuff

--- sources/main.c
+++ sources/main.c
@@ -1,11 +1,21 @@
 #include "raylib.h"
 
+#define BOARD_SIZE (9)
 #define SCREEN_WIDTH (800)
 #define SCREEN_HEIGHT (450)
 
 #define WINDOW_TITLE "TicTacToe"
 
 Texture2D texture;
+const int distance = 40;
+
+typedef struct {
+    char* value;
+    int x;
+    int y;
+} field;
+
+field board[BOARD_SIZE];
 
 void draw() {
     BeginDrawing();
@@ -20,18 +30,42 @@ void draw() {
     const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
     DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
 
+    for (int i = 0; i < sizeof(board) / sizeof(board[0]); ++i) {
+        field value = board[i];
+        const char* textToDraw = value.value;
+        DrawText(textToDraw, SCREEN_WIDTH / 2 + value.x * distance, SCREEN_HEIGHT / 2 + value.y * distance, 20, BLACK);
+    }
+
     EndDrawing();
 }
 
 void update(float deltaTime) {
+    if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+        Vector2 pos = GetMousePosition();
 
+        for (int i = 0; i < sizeof(board) / sizeof(board[0]); ++i) {
+            field value = board[i];
+            Rectangle rect = {SCREEN_WIDTH / 2 + value.x * distance, SCREEN_HEIGHT / 2 + value.y * distance, distance, distance};
+            if (CheckCollisionPointRec(pos, rect)) {
+                board[i].value = "X";
+            }
+        }
+    }
 }
 
 void init() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
     SetTargetFPS(60);
 
-    texture = LoadTexture(ASSETS_PATH"test.png"); // Check README.md for how this works
+    texture = LoadTexture(ASSETS_PATH"test.png"); // Check README.md for how this works.
+
+    for (int x = 0; x < BOARD_SIZE / 3; ++x) {
+        for (int y = 0; y < BOARD_SIZE / 3; ++y) {
+            char* text = "h";
+            field value = {text, x, y};
+            board[y * 3 + x] = value;
+        }
+    }
 }
 
 int deInit() {
